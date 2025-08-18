fd2855c8472229a7bebda33dcfd1d7678fe620f7
Paul Spasojevic <paulspas27@gmail.com>
2025-08-18 22:15:39 +1000

    30ms

--- gtfs_files/real_walking_distances.R
+++ gtfs_files/real_walking_distances.R
@@ -1,15 +1,20 @@
+#download TR road from Data VIC.
 tr_road <- read_sf('sf_input/tr_road/Order_I2F4YY/mga2020_55/esrishape/customised_delivery/MELBOURNE_WATER-0/VMTRANS/TR_ROAD_ALL.shp')
 tr_road = tr_road %>% st_transform('wgs84')
 
+#download TR road infra from Data VIC.
 tr_road_infra <- read_sf('sf_input/tr_road_infrastructure/Order_08APF2/mga2020_55/esrishape/customised_delivery/MELBOURNE_WATER-0/VMTRANS/TR_ROAD_INFRASTRUCTURE.shp')
 tr_road_infra <- tr_road_infra %>% st_transform('wgs84')
 
+# i restrict my sample to Brunswick, just so that I can test it.
 brunswick <- getbb("brunswick, victoria, australia", format_out = 'sf_polygon') %>%
   st_transform('wgs84')
 
+#find intersections between sample area and larger dfs
 tr_road_brunswick <- st_intersection(tr_road, brunswick)
 tr_infra_brunswick <- st_intersection(tr_road_infra, brunswick)
 
+#only select columns I want.
 tr_road_brunswick = tr_road_brunswick %>%
   select(FROM_UFI, TO_UFI)
 
@@ -17,7 +22,6 @@ tr_infra_brunswick = tr_infra_brunswick %>%
   select(UFI)
 
 ## create vertices
-
 vertices <- tr_infra_brunswick %>%
   mutate(UFI = as.character(UFI))
 
@@ -36,36 +40,34 @@ edges_dt <- bind_rows(edges, edges_reversed) %>%
   summarise(distance = min(distance), .groups = 'drop') %>%
   as.data.table()
 
-setkey(edges_dt, FROM_UFI)
+edges_list = split(edges_dt, edges_dt$FROM_UFI)
 
-#map network to triple check structure
+#map network to triple
 plot(st_geometry(edges), col = 'blue', lwd = 2)
 plot(st_geometry(vertices), col = 'red', add = TRUE, pch = 16, cex = 1.5)
 
+#
 
 
-djikstra <- function(starting_node, max_distance = 46 * 84) {
+dijkstra <- function(starting_node, max_distance = 46 * 84) {
 
-  profvis({
+  #profvis({
 
-    # Set up tracking df
-    djikstra_tracking <- vertices %>%
-      st_drop_geometry() %>%
-      mutate(distance = Inf, visited = F) %>%
-      as.data.table()
-
-    setkey(djikstra_tracking, UFI)
-    djikstra_tracking[starting_node]$distance <- 0
+    # Set up tracking with named vectors for O(1) lookups
+    distances <- setNames(rep(Inf, nrow(vertices)), vertices$UFI)
+    distances[starting_node] <- 0
+    visited <- setNames(rep(FALSE, nrow(vertices)), vertices$UFI)
 
     # Use a simple vector to track unvisited nodes
-    unvisited_nodes <- djikstra_tracking$UFI
+    unvisited_nodes <- vertices$UFI
+
 
     while (length(unvisited_nodes) > 0) {
       # Find closest unvisited node
-      distances <- djikstra_tracking[unvisited_nodes]$distance
-      min_idx <- which.min(distances)
+      unvisited_distances <- distances[unvisited_nodes]
+      min_idx <- which.min(unvisited_distances)
       current_node <- unvisited_nodes[min_idx]
-      current_tracked_distance <- distances[min_idx]
+      current_tracked_distance <- unvisited_distances[min_idx]
 
       # Early termination if we reach max distance
       if(current_tracked_distance > max_distance) {
@@ -76,37 +78,41 @@ djikstra <- function(starting_node, max_distance = 46 * 84) {
       unvisited_nodes <- unvisited_nodes[-min_idx]
 
       #update master tracking branch
-      djikstra_tracking[current_node, visited := TRUE]
+      visited[current_node] <- TRUE
 
       #find what we can get to
-      reachable_nodes <- edges_dt[current_node]
+      reachable_nodes <- edges_list[[current_node]]
+      #reachable_nodes <- edges_dt[current_node]
 
       #vectorised for speed
       if(nrow(reachable_nodes) > 0) {
-        neighbor_distances <- djikstra_tracking[reachable_nodes$TO_UFI]$distance
+        neighbor_distances <- distances[reachable_nodes$TO_UFI]
         new_distances <- reachable_nodes$distance + current_tracked_distance
         update_mask <- new_distances < neighbor_distances & !is.na(neighbor_distances)
 
         if(any(update_mask)) {
-          djikstra_tracking[reachable_nodes$TO_UFI[update_mask], distance := new_distances[update_mask]]
+          distances[reachable_nodes$TO_UFI[update_mask]] <- new_distances[update_mask]
         }
       }
 
     }
 
-    djikstra_tracking[, walking_time := distance %/% 84]
-    djikstra_tracking = djikstra_tracking[visited == T]
+    # Create result data.table
+    result <- data.table(
+      UFI = names(distances)[visited],
+      distance = distances[visited],
+      walking_time = distances[visited] %/% 84,
+      visited = TRUE
+    )
 
+  #})
 
-  })
-
-
-  return(djikstra_tracking)
+  return(result)
 
 }
 
 
-djikstra("2293277") -> d
+dijkstra("2290274") -> d
 
 d %>% filter(visited) %>% left_join(tr_infra_brunswick %>% mutate(UFI = as.character(UFI)), by = "UFI") -> djik_test
 
